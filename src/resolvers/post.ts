5c6f2f99b71920aafe3934a4211ed2a7764788de
Saumya Pandey <sp160899@gmail.com>
2020-11-10 23:55:12 +0530

    Expose creator field and update posts query to send creator details

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -29,19 +29,35 @@ export class PostResolver {
   ): Promise<PaginatedPosts> /*ts*/ {
     const realLimit = Math.min(20, limit);
     const realLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("p")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimitPlusOne);
+
+    const replacements: any[] = [realLimitPlusOne];
 
     if (cursor) {
-      qb.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor))
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
-    const posts = await qb.getMany();
 
+    //there can be multple user table
+    let posts = await getConnection().query(
+      `
+      select p.*, json_build_object('id',u.id,'username',u.username,'email',u.email) creator from post p 
+      inner join public.user u on u.id = p."creatorId"
+      ${cursor ? `where p."createdAt" < $2` : ""}
+      order by p."createdAt" DESC
+      limit $1
+    `,
+      replacements
+    );
+    // posts = posts.map((p) => {
+    //   const { username, email, ...post } = p;
+    //   return {
+    //     ...post,
+    //     creator: {
+    //       username,
+    //       email
+    //     }
+    //   };
+    // });
+    // console.log(posts);
     return {
       posts: posts.slice(0, realLimit),
       hasMore: posts.length === realLimitPlusOne
